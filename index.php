984a1dfaaa69d752d0ca8a4e4d1a8a37879f7584
DUNJUNPC\dunJun <francisco.p.pimenta@outlook.com>
2026-03-12 12:05:39 +0000

    New Project Start

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+    <script type="importmap">
+        {
+      "imports": {
+        "three": "https://cdn.jsdelivr.net/npm/three@0.149.0/build/three.module.js",
+        "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.149.0/examples/jsm/"
+      }
+    }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>ProjetoFinal</title>
+</head>
+
+<body>
+    <div class="container-fluid" id="main">
+        <div class="row">
+            <div class="col-6 min-vh-100" id="left">
+                <canvas id="c"></canvas>
+            </div>
+            <!-- justify-content-center centrado horizontalmente align-items-start para nao ocupar o espaco todo da col-->
+            <div class="col-6 min-vh-100 d-flex justify-content-center align-items-start" id="right">
+                <div class="container">
+                    <div class="row">
+                        <img src="assets/ipvc_Symbol.png" id="symbol" alt="ipvc">
+                    </div>
+                    <div class="row">
+                        <p id="welcome" class="text-center">Bem-vindo ao IPVC. O
+                            <strong>TEU</strong> ponto de partida.
+                        </p>
+                    </div>
+                    <div class="row">
+                        <h2 id="schools" class="text-center">Escolas</h2>
+                    </div>
+                    <div class="row"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="module" src="js/index.js"></script>
+</body>
+
+</html>
---
+++ js/index.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+// import * as Text from './text.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import * as Moving from './movingobjects.js';
+export let camera,scene, renderer, canvas,cubeSize=2,clock,controls,sphere,loader3D = new GLTFLoader(),loaderText = new FontLoader(),negative_infinity = -100000,model,models=[],movingmodels=[],buttonZoom;
+let getEventos = false,getUO=false;
+let variableButton,container;            
+
+function init( ){
+    canvas = document.getElementById('c');
+    const leftSide = document.getElementById("left");
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xD9D9D9);
+
+    renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+
+    camera = new THREE.PerspectiveCamera(45,2,0.1,30);
+    camera.position.set(0,0,10);
+
+    resizeToLeft();
+
+    window.addEventListener('resize',()=> requestAnimationFrame(resizeToLeft));
+
+    const ReObserver = new ResizeObserver(()=>requestAnimationFrame(resizeToLeft));
+    ReObserver.observe(leftSide);
+
+    animate();
+
+    function resizeToLeft(){
+        const width = leftSide.clientWidth;
+        const height = leftSide.clientHeight;
+
+        if (width <= 0 || height <= 0) return;
+
+        const size = new THREE.Vector2();
+        renderer.getSize(size);
+        if(size.x === width && size.y === height) return;
+
+        renderer.setSize(width,height,false);
+
+        camera.aspect = width/height;
+        camera.updateProjectionMatrix();
+    }
+
+    function animate(){
+        requestAnimationFrame(animate);
+        renderer.render(scene,camera);
+    }
+    
+}
+
+init();
