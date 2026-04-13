a611a5385176ae52d41cb611ef70d262959919f3
DUNJUNPC\dunJun <francisco.p.pimenta@outlook.com>
2026-04-13 16:33:18 +0100

    Logout Sucessfully

--- admin/cursos/edit.php
+++ admin/cursos/edit.php
@@ -523,7 +523,7 @@ $textura = $_SESSION['curso']['Textura'];
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="../../database/admin/logout.php">Logout</a>
                 </div>
             </div>
         </div>
